3947dc82f5d9ba15c095c85f22349f493a6b4bab
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2023-04-06 18:17:56 +0800

    update project name

--- frontend/plugins/global-variables.ts
+++ frontend/plugins/global-variables.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin(nuxtApp => {
     return {
       provide: {
         test: (msg: string) => `test ${msg}!`,
-        baseUrl: process.server ? 'http://classrushsystem-backend-1:5000/api/' : 'http://localhost:5000/api/'
+        baseUrl: process.server ? 'http://courseintegration-backend-1:5000/api/' : 'http://localhost:5000/api/'
       }
     }
 })
