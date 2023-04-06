e845e26df0edbfa5359431601dc5be425fac6a87
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2023-04-06 18:02:54 +0800

    update project name

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,8 @@ from app.app import app
 from pymongo import MongoClient
 
 if __name__ == '__main__':
-    client = MongoClient("mongodb://classrushsystem-mongodb-1:27017/")
-    db = client["classrushsystem"]
+    client = MongoClient("mongodb://courseintegration-mongodb-1:27017/")
+    db = client["courseintegration"]
     if "users" not in db.list_collection_names():
         db.create_collection("users")
     app.run(host='0.0.0.0')
